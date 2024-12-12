0d9c8a28dce378ed5c3c3ff994cb4349a0d92f56
ashleyjsilvax <ashleyjsilvax@gmail.com>
2024-12-12 14:39:08 -0500

    Created CircleShape and Player classes and updated main

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
-#importing required files
+# importing required files
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
+    # initalization of pygame
     pygame.init()   
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    # creation of variables needed in main
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    # game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
         screen.fill("black")
+        player.draw(screen)
+        
+        # Will update the contents of the entire display
         pygame.display.flip()
+        
+        # limits framerate to 60 FPS
+        dt = clock.tick(60) /1000
     
     
 
